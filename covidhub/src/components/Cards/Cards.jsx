59d2d243c4108fc9dead9d0fc4a74dd2903537a8
Syndicate555 <saziz028@uottawa.ca>
2020-06-30 01:35:10 -0400

    Update Cards.jsx

--- covidhub/src/components/Cards/Cards.jsx
+++ covidhub/src/components/Cards/Cards.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import "./Cards.module.css";
 const Cards = (props) => {
   console.log(props);
-  return <h1>Cards</h1>;
+  return (
+    <div className="container">
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography></Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Cards;
