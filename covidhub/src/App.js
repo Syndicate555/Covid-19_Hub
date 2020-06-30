07da61e371f30d438689063e18810cd4793bc093
Syndicate555 <saziz028@uottawa.ca>
2020-06-29 22:43:12 -0400

    Update App.js

--- covidhub/src/App.js
+++ covidhub/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./App.css";
+import "./App.css";
 import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
@@ -7,7 +7,7 @@ import CountryPicker from "./components/CountryPicker/CountryPicker";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <Cards />
         <CountryPicker />
         <Chart />
