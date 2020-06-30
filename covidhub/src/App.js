a9f1874f93f003279f4d922da264360b7911e4ed
Syndicate555 <saziz028@uottawa.ca>
2020-06-29 22:36:04 -0400

    bugs

--- covidhub/src/App.js
+++ covidhub/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
 import Cards from "./components/Cards/Cards";
-import Chart from "./components/Cards/Chart";
-import CountryPicker from "./components/Cards/CountryPicker";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
 
 class App extends React.Component {
   render() {
