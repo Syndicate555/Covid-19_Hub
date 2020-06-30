ed40e1a56b0746b4dc8018872c0d8021a8599d5b
Syndicate555 <saziz028@uottawa.ca>
2020-06-29 21:26:44 -0400

    Update App.js

--- covidhub/src/App.js
+++ covidhub/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
 import "./App.css";
 
-function App() {
-  return <div>Hello World</div>;
+class App extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
 }
 
 export default App;
