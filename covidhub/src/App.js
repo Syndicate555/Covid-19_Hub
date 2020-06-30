5ad37880be5e9f9280fc518fd08595dd2be44ddf
Syndicate555 <saziz028@uottawa.ca>
2020-06-29 23:49:37 -0400

    changes

--- covidhub/src/App.js
+++ covidhub/src/App.js
@@ -3,8 +3,13 @@ import "./App.css";
 import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
+import { fetchData } from "./api";
 
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
   render() {
     return (
       <div className="container">
--- covidhub/src/api/index.js
+++ covidhub/src/api/index.js
@@ -1,3 +1,21 @@
 import axios from "axios";
 
-const url = "https://covid19.mathdro.id/api";
+const stats = "https://covid19.mathdro.id/api";
+const country = "https://covid19.mathdro.id/api/countries";
+
+export const fetchData = async () => {
+  try {
+    const { data } = await axios.get(stats);
+    const modifiedData = {
+      confirmed: data.confirmed,
+      recovered: data.recovered,
+      deaths: data.deaths,
+      lastUpdate: data.lastUpdate,
+    };
+
+    const countryData = await axios.get(country);
+    return { modifiedData, countryData };
+  } catch (error) {
+    console.log(error);
+  }
+};
