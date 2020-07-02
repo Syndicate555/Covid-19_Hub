3afa77c976142f1d48950b2ff4baea3463082c1a
Syndicate555 <saziz028@uottawa.ca>
2020-07-01 22:56:08 -0400

    Update Chart.jsx

--- covidhub/src/components/Chart/Chart.jsx
+++ covidhub/src/components/Chart/Chart.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { fetchDailyData } from "../../api";
 import { Line, Bar } from "react-chartjs-2"; // also need to install chart.js seperately
+import styles from "./Chart.module.css";
 const Chart = () => {
   const [dailyData, setDailyData] = useState({});
   useEffect(() => {
@@ -9,6 +10,16 @@ const Chart = () => {
     };
     fetchAPI();
   });
+
+  // first wait for the data to be fetched from the API, then return the result. Until then, return null
+  const lineChart = dailyData[0] ? (
+    <Line
+      data={{
+        labels: "",
+        datasets: [{}, {}],
+      }}
+    />
+  ) : null;
   return <h1>Chart</h1>;
 };
 
