b29876b723e208f3cf9c11517f667ea138f2e35e
Syndicate555 <saziz028@uottawa.ca>
2020-07-01 23:21:26 -0400

    Create Footer.jsx

---
+++ covidhub/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./recipe.module.css";
+
+function Footer(props) {
+  return (
+    <footer className={style.footer}>
+      Created by <a href="https://www.saffataziz.site/"> Saffat Aziz</a>
+    </footer>
+  );
+}
+
+export default Footer;
