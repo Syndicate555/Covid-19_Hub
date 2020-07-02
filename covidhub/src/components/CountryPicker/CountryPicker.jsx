e7181aa4624627688f08ea0f7dedebacd256d555
Syndicate555 <saziz028@uottawa.ca>
2020-07-02 02:17:42 -0400

    changes

--- covidhub/src/components/CountryPicker/CountryPicker.jsx
+++ covidhub/src/components/CountryPicker/CountryPicker.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { NativeSelect, FormControl } from "@material-ui/core";
+import styles from "./CountryPicker.module.css";
 const CountryPicker = () => {
   return;
   <FormControl className={styles.formControl}>
